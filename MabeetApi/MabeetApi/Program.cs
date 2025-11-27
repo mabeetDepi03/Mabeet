0a4e0255a5b3a2864d0465ec73665b5d48a1eac9
Mostafa <whoami.mostafa@gmail.com>
2025-11-27 22:38:47 +0200

    update programCs

--- MabeetApi/MabeetApi/Program.cs
+++ MabeetApi/MabeetApi/Program.cs
@@ -5,7 +5,7 @@ using MabeetApi.Services.Admin;
 using MabeetApi.Services.Admin.Accommodations;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-
+// Update program
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
